2ccc0087a5d13fd14ea4f17d6142bd68139a57b8
ozgeterzioglu <ozgeterzioglu@yahoo.com>
2023-02-21 22:38:39 -0800

    next: save links as .html to data/raw

--- scripts/fda.py
+++ scripts/fda.py
@@ -8,11 +8,15 @@ request = requests.get(url)
 request.raise_for_status()
 site = BeautifulSoup(request.text, features = "lxml")
 
+#def collect_links():
+
 urls = []
 for link in site.find_all("a", attrs = {"href": re.compile("^/medical-devices/medical-device-recalls/")}):
     result = link.get("href")
-    links = urls.append(result) #it only prints the unique ids for each url, i would need to figure out how to grab the whole link (starting with https://)
-    #next: take that link then add it to data/raw/name of unique id.html
+    full_link = "https://www.fda.gov" + result
+    urls.append(full_link)
+    
+    #next: take result then add it to data/raw/name of unique id.html
 
 
 #from pathlib import Path
@@ -20,11 +24,5 @@ for link in site.find_all("a", attrs = {"href": re.compile("^/medical-devices/me
 #file = os.path.join(cwd, "data/raw")
 
 
-#def collect_links():
-
-
 #if __name__ == "__main__":
     #collect_links()
-
-
-
